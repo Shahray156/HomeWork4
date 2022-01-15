906b85fc8fd209fe4fa45bf3cdcc8b169634965f
Shahray156 <shahray1561@gmail.com>
2022-01-15 21:16:24 +0300

    последний коммит

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,9 +19,8 @@ public class Main {
         // Задача № 2
 
         int[] arraysOne = new int[]{1, 2, 3};
-        arraysOne[0] = 1;
         for (int i = 0; i < arraysOne.length; i++) {
-            if (arraysOne[i] == arraysOne.length) {
+            if (i == arraysOne.length - 1) {
                 System.out.print(arraysOne[i]);
             } else {
                 System.out.print(arraysOne[i] + ",");
@@ -31,9 +30,8 @@ public class Main {
         System.out.println();
 
         double[] boxes1 = new double[]{1.57, 7.654, 9.986};
-        boxes1[0] = 1.57;
-        for (int a = 0; a > boxes1.length; a++) {
-            if (boxes1[a] == boxes1.length) {
+        for (int a = 0; a < boxes1.length; a++) {
+            if (a == boxes1.length - 1) {
                 System.out.print(boxes1[a]);
             } else {
                 System.out.print(boxes1[a] + ",");
@@ -43,9 +41,8 @@ public class Main {
         System.out.println();
 
         int[] arrays3 = {5, 6, 7, 8, 9};
-        arrays3[0] = 5;
-        for (int y = 0; y > arrays3.length; y++) {
-            if (arrays3[y] == arrays3.length) {
+        for (int y = 0; y < arrays3.length; y++) {
+            if (y == arrays3.length - 1) {
                 System.out.print(arrays3[y]);
             } else {
                 System.out.print(arrays3[y] + ",");
@@ -58,7 +55,7 @@ public class Main {
 
         int[] arrays4 = new int[]{1, 2, 3};
         for (int i = arrays4.length - 1; i >= 0; i--) {
-            if (i == arrays4.length - 3) {
+            if (i == 0) {
                 System.out.print(arrays4[i]);
             } else {
                 System.out.print(arrays4[i] + ",");
@@ -69,7 +66,7 @@ public class Main {
 
         double[] boxes2 = new double[]{1.57, 7.654, 9.986};
         for (int a = boxes2.length - 1; a >= 0; a--) {
-            if (a == boxes2.length) {
+            if (a == 0) {
                 System.out.print(boxes2[a] + " ");
             } else {
                 System.out.print(boxes2[a] + ",");
@@ -80,7 +77,7 @@ public class Main {
 
         int[] arrays5 = {5, 6, 7, 8, 9};
         for (int y = arrays5.length - 1; y >= 0; y--) {
-            if (y == arrays5.length - 5) {
+            if (y == 0) {
                 System.out.print(arrays5[y] + " ");
             } else {
                 System.out.print(arrays5[y] + ",");
@@ -92,11 +89,14 @@ public class Main {
         int[] arrays6 = new int[]{1, 2, 3};
         for (int d = 0; d < arrays6.length; d++) {
             if (arrays6[d] % 2 != 0) {
-                System.out.print(arrays6[d] + 1 + ",");
+                System.out.print(arrays6[d] + 1);
             } else {
-                System.out.print(arrays6[d] + ",");
-            }
-
+                System.out.print(arrays6[d]); }
+            if (d != arrays6.length - 1) {
+                System.out.print(","); }
+            else {
+                System.out.print(" ");
+                    }
+                }
             }
         }
-    }
